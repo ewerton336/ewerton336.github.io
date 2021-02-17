6593196dcd91634c3f4220f42e0902d376bc3ccb
ewerton336 <65933715+ewerton336@users.noreply.github.com>
2021-02-17 11:57:22 -0300

    Add files via upload
    Corrigido problema com a utilização de javascript. Criado 3 colunas para Conhecimentos em programação, certificados e outros. Código formatado para melhor visualização

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function ao_clicar() {
+  alert("Em desenvolvimento");
+}
+
+function alterar_elemento() {
+  document.getElementById("agradecimento").innerHTML = "ooiiii";
+}
+
+function retorna_elemento() {
+  document.getElementById("agradecimento").innerHTML = "a";
+}
+
+function mudarTema() {
+  document.getElementById("corpo").classList.add = "text-dark bg-light";
+}
+
+// function yesnoCheck(that) {
+//     if (that.value == "other") {
+//         document.getElementById("ifYes").style.display = "block";
+//     } else {
+//         document.getElementById("ifYes").style.display = "none";
+//     }
